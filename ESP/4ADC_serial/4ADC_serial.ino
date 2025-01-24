523739e8dd2dbc5f2fcd51b7bf1b02b6077356c3
Alexandr Ivanov <sailtothe54@gmail.com>
2025-01-24 15:16:42 +0300

    Add esp

---
+++ ESP/4ADC_serial/4ADC_serial.ino
@@ -0,0 +1,30 @@
+// Входы АЦП
+const int potPin1 = 34;
+const int potPin2 = 35;
+const int potPin3 = 25;
+const int potPin4 = 26;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Считываем значения с четырёх потенциометров
+  int val1 = analogRead(potPin1);
+  int val2 = analogRead(potPin2);
+  int val3 = analogRead(potPin3);
+  int val4 = analogRead(potPin4);
+
+  // Выводим результаты в монитор порта
+  Serial.print("P1: ");
+  Serial.print(val1);
+  Serial.print("\tP2: ");
+  Serial.print(val2);
+  Serial.print("\tP3: ");
+  Serial.print(val3);
+  Serial.print("\tP4: ");
+  Serial.println(val4);
+
+  // Делаем небольшую задержку для удобства наблюдения
+  delay(200);
+}
